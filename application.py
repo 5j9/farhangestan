28b38f16887de4d2d3b60ba7501a5208367eea43
irdb <5j9@users.noreply.github.com>
2015-05-27 15:31:48 +0430

    first release of the application and related files

---
+++ application.py
@@ -0,0 +1,83 @@
+import sqlite3
+
+from flask import Flask
+from flask import g
+from flask import request
+from flask import redirect, url_for
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def searchform():
+    return redirect(url_for('static', filename='searchform.html'))
+
+
+@app.route('/results')
+def searchresult():
+    args = request.args
+    daftar = args.get('daftar', '')
+    query = (
+        "SELECT * FROM words WHERE "
+        "(mosavab LIKE ? OR "
+        "biganeh LIKE ? OR "
+        "tarif LIKE ?)"
+        " AND "
+        "(mosavab LIKE ? OR "
+        "biganeh LIKE ? OR "
+        "tarif LIKE ?)"
+        " AND "
+        "(mosavab LIKE ? OR "
+        "biganeh LIKE ? OR "
+        "tarif LIKE ?)"
+        " AND "
+        "hozeh LIKE ?"
+        " AND "
+        "daftar LIKE ?"
+        " LIMIT 100;"
+    )
+    word = args.get('word', '')
+    wordstart = args.get('wordstart', '')
+    wordend = args.get('wordend', '')
+    hozeh = args.get('hozeh', '')
+    daftar = int(daftar) if daftar.isnumeric() else ''
+    
+    rows = query_db(
+        query,
+        ('%{}%'.format(word),)* 3 + ('{}%'.format(wordstart),)* 3 +
+        ('%{}'.format(wordend),)* 3 + ('%{}%'.format(hozeh),) +
+        ('%{}%'.format(daftar),),
+    )
+    
+    return render_template('results.html', rows=rows)
+
+
+def connect_to_db():
+    """Connects to the specific database."""
+    return sqlite3.connect('farhangestan.sqlite3')
+
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = connect_to_db()
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return rv
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,65 @@
+﻿function daftar(s) {
+    switch(s) {
+        case '1':
+            return 'نخست';
+        case '2':
+            return 'دوم';
+        case '3':
+            return 'سوم';
+        case '4':
+            return 'چهارم';
+        case '5':
+            return 'پنجم';
+        case '6':
+            return 'ششم';
+        case '7':
+            return 'هفتم';
+        case '8':
+            return 'هشتم';
+        case '9':
+            return 'نهم';
+        case '10':
+            return 'دهم';
+        case '11':
+            return 'یازدهم';
+        case '12':
+            return 'دوازدهم';
+        case '13':
+            return 'سیزدهم';
+        case '14':
+            return 'چهاردهم';
+        case '15':
+            return 'پانزدهم';
+    } 
+}
+
+function num2fa(string) {
+    newstring = '';
+    $(string.split('')).each(function() {
+        newstring += String.fromCharCode(this.charCodeAt(0) + 1728)
+    });
+    return newstring
+}
+
+function copy_citation(text) {
+  window.prompt("برای یادکرد در ویکی‌پدیا رونوشت بردارید:", text);
+}
+
+$(function(){
+    $("tr").slice(1).click(
+      function(){ 
+        tds = $(this).find('td')
+        c = '<ref>{{یادکرد فرهنگستان | مصوب=';
+        c += tds[0].textContent;
+        c += ' |بیگانه=';
+        c += tds[1].textContent;
+        c += ' |حوزه=';
+        c += tds[2].textContent.replace(/[\[\]]/g, '');
+        c += ' |دفتر=';
+        c += daftar(tds[4].textContent);
+        c += ' }}</ref>';
+        copy_citation(c); 
+      }
+    );
+    $('ul').prepend('<li>برای به دست آوردن <a href="https://fa.wikipedia.org/wiki/%D8%A7%D9%84%DA%AF%D9%88:%DB%8C%D8%A7%D8%AF%DA%A9%D8%B1%D8%AF-%D9%81%D8%B1%D9%87%D9%86%DA%AF%D8%B3%D8%AA%D8%A7%D9%86">یادکرد فرهنگستان</a> جهت استفاده در ویکی‌پدیا کافیست روی ردیفی که می‌خواهید یادکرد آن ساخته شود کلیک کنید.</li>');
+})
