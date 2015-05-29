2d7b0f4dafbea8af933495c08d6c3430f7277779
irdb <5j9@users.noreply.github.com>
2015-05-30 00:58:21 +0430

    parsing synonym part of definition

--- static/script.js
+++ static/script.js
@@ -45,21 +45,89 @@ function copy_citation(text) {
   window.prompt("برای یادکرد در ویکی‌پدیا رونوشت بردارید:", text);
 }
 
+function nonum_mosavab(mosavab) {
+    if (!/\d\d$/.test(mosavab) && !/ \d$/.test(mosavab)) {
+        return mosavab.replace(/\d$/, '');
+    }
+    return mosavab;
+}
+
+function nonum_motaradef(motaradef) {
+    if (!/\d\d$/.test(motaradef)) {
+        return motaradef.replace(/ \d$/, '').replace(/ \d،/, '،');
+    }
+    return motaradef;
+}
+
+function nonum_biganeh(biganeh) {
+    return biganeh.replace(/ \d(?=, | \(| ?\/ |$)/g, '')
+}
+
 $(function(){
     $("tr").slice(1).dblclick(
       function(){ 
         tds = $(this).find('td')
         c = '<ref>{{یادکرد فرهنگستان | مصوب=';
-        mosavab = tds[0].textContent
-        if (!/\d\d$/g.test(mosavab) && !/ \d$/g.test(mosavab)) {
-            mosavab = mosavab.replace(/\d$/g, '');
+        mosavab = nonum_mosavab(tds[0].textContent)
+        tarif = tds[3].textContent
+        if (tarif.indexOf('متـ . ') !== -1) {
+            //tarif has at list one synonym
+            postmot = tarif.split('متـ . ')[1];
+            if (postmot.indexOf('*') !== -1) {
+                // the star indicates some kind of comment.
+                // e.g. واژه * مصوب فرهنگستان اول
+                // also see سرگذشت خود
+                postmot = nonum_motaradef(postmot.split('*')[0].trim());
+            }
+            if (!/\w{2,}/i.test(postmot)) {
+                //postmot only contains farsi synonym
+                c += mosavab + '، ' + nonum_motaradef(postmot);
+            }
+            else {
+                //postmot contains latin and farsi synonyms
+                c += mosavab;
+                c += ' | مصوب مترادف=';
+                // may contain newline. see واکنشگاه هسته‌ای
+                motparts = postmot.split('\n');
+                firsttime = true;
+                for (i=0; i < motparts.length; i++) {
+                    part = motparts[i].trim()
+                    if (!part) continue
+                    // [\u0600-\u06FF] is the Arabic (Unicode block)
+                    // https://en.wikipedia.org/wiki/Arabic_%28Unicode_block%29
+                    mosavab_motaradef = /[\u0600-\u06FF \u200c]+/.exec(part);
+                    if (!mosavab_motaradef) continue;
+                    mosavab_motaradef = mosavab_motaradef[0].trim();
+                    if (!mosavab_motaradef) continue;
+                    if (firsttime) {
+                        c += nonum_mosavab(mosavab_motaradef);
+                        firsttime = false;
+                    }
+                    else {
+                        c += '، ' + nonum_mosavab(mosavab_motaradef);
+                    }
+                }
+                c += ' | بیگانهٔ مترادف=';
+                firsttime = true
+                for (i=0; i < motparts.length; i++) {
+                    part = motparts[i].trim();
+                    if (!part) continue;
+                    biganeh_motaradef = /\w{2,}[ \w-]*(?=$|[\u0600-\u06FF\u200c])/.exec(part);
+                    if (!biganeh_motaradef) continue;
+                    biganeh_motaradef = biganeh_motaradef[0].trim();
+                    if (!biganeh_motaradef) continue;
+                    if (firsttime) {
+                        c += nonum_biganeh(biganeh_motaradef);
+                        firsttime = false;
+                    }
+                    else {
+                        c += ', ' + nonum_biganeh(biganeh_motaradef);
+                    }                    
+                }
+            }
         }
-        c += mosavab;
         c += ' | بیگانه=';
-        biganeh = tds[1].textContent;
-        if (!/\d$/g.test(mosavab)) {
-            biganeh = biganeh.replace(/ \d(?=, | \(| ?\/ |$)/g, '');
-        }
+        biganeh = nonum_biganeh(tds[1].textContent);
         c += biganeh;
         c += ' | حوزه=';
         c += tds[2].textContent.replace(/[\[\]]/g, '');
